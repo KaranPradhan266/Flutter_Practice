29a6a6ec39ff71079984273457ea333fbaaff97f
Karan <karanpradhan266@gmail.com>
2025-04-27 12:18:13 -0700

    Worked with columns

--- lib/home.dart
+++ lib/home.dart
@@ -8,11 +8,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('My Coffee Shop'),
+        title: const Text('My Coffee Shop',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: const Text('hi world'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20.0),
+            child: const Text('How I Like My coffee...!'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20.0),
+            child: const Text('Coffee prefs'),
+          )
+        ],
+      ),
     );
   }
 }
